3a9548edae6ac488e87d89a017415b1671125727
saharpgh <saharpourghasemi02@gmail.com>
2024-07-10 17:36:28 +0330

    change src file name

---
+++ docs/js/script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".progress-slide-carousel", {
+  loop: true,
+  fraction: true,
+  autoplay: {
+    delay: 1200,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".progress-slide-carousel .swiper-pagination",
+    type: "progressbar",
+  },
+  });
